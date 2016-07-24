1752834f985c22eb7ec69344f4e78eb2fec9b4d4
Ronny Wang <ronnywang@gmail.com>
2016-07-24 18:07:30 +0800

    加上 session 支援

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 include(__DIR__ . '/webdata/init.inc.php');
 
+Pix_Session::setAdapter('cookie', array('secret' => getenv('SESSION_SECRET'), 'cookie_key' => 'SESSION_OPENGOVCONTACT'));
 Pix_Controller::addCommonHelpers();
 Pix_Controller::dispatch(__DIR__ . '/webdata/');
