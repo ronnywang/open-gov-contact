5051679143e3d0b7599c8001db596c8bd59e0e50
Ronny Wang <ronnywang@gmail.com>
2016-07-24 18:07:52 +0800

    改用支援 general 的 DATABASE_URL 的寫法

--- webdata/init.inc.php
+++ webdata/init.inc.php
@@ -16,22 +16,4 @@ Pix_Table::setLongQueryTime(3);
 // TODO: 之後要搭配 geoip
 date_default_timezone_set('Asia/Taipei');
 
-
-if (preg_match('#pgsql://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('PGSQL_DATABASE_URL')), $matches)) {
-    $options = array(
-        'host' => $matches[3],
-        'user' => $matches[1],
-        'password' => $matches[2],
-        'dbname' => $matches[4],
-    );
-    Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_PgSQL($options));
-} else if (preg_match('#mysql://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('MYSQL_DATABASE_URL')), $matches)) {
-    $db = new StdClass;
-    $db->host = $matches[3];
-    $db->username = $matches[1];
-    $db->password = $matches[2];
-    $db->dbname = $matches[4];
-    $config = new StdClass;
-    $config->master = $config->slave = $db;
-    Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_MysqlConf(array($config)));
-}
+Pix_Table_Db::addDbFromURI(getenv('DATABASE_URL'));
