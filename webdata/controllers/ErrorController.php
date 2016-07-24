c9ee3103064f0ebe8bb34210bb704e7e84014202
Ronny Wang <ronnywang@gmail.com>
2016-07-24 18:08:18 +0800

    要讓預設的 Error 照實噴正確的 code

--- webdata/controllers/ErrorController.php
+++ webdata/controllers/ErrorController.php
@@ -5,9 +5,11 @@ class ErrorController extends Pix_Controller
     public function errorAction()
     {
         if ($this->view->exception instanceof Pix_Controller_Dispatcher_Exception) {
+            header('404 Not Found', true, 404);
             echo '404';
             return $this->noview();
         } else {
+            header('500', true, 500);
             trigger_error(strval($this->view->exception), E_USER_WARNING);
             return $this->noview();
         }
